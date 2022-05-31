980081537c0520a9a2b5ba59875c412fdb596def
bobyzzy <ziyodullayev2000@mail.ru>
2022-06-01 00:49:16 +0300

    add create update get operation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopFront.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,51 @@ namespace ShopFront
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Product> products = new List<Product>();
+        private Product NewProduct = new Product();
+        private string OldName;
+        private Product SelectedProduct = new Product();
+
         public MainWindow()
         {
             InitializeComponent();
+            GetProducts();
+
+            AddNewProductGrid.DataContext = NewProduct;
+        }
+
+        private void GetProducts()
+        {
+           ShopProduct.ItemsSource = null;
+           ShopProduct.ItemsSource = products;
+        }
+
+        private void AddProduct(object s, RoutedEventArgs e)
+        {
+            products.Add(new Product { ImageUrl = NewProduct.ImageUrl, Name = NewProduct.Name });
+            NewProduct = new Product();
+            AddNewProductGrid.DataContext = NewProduct;
+
+            GetProducts();
+        }
+
+        private void UpdateProductForEdit(object s, RoutedEventArgs e)
+        {
+            SelectedProduct = (s as FrameworkElement).DataContext as Product;
+            OldName = SelectedProduct.Name;
+            UpdateProductGrid.DataContext= SelectedProduct;
+        }
+
+        private void UpdateProduct(object s, RoutedEventArgs e)
+        {
+            var product = products.FirstOrDefault(p => p.Name == OldName);
+            if (product != null)
+            {
+                product.Name = SelectedProduct.Name;
+                product.ImageUrl = SelectedProduct.ImageUrl;
+            }
+
+            GetProducts();
         }
     }
 }
